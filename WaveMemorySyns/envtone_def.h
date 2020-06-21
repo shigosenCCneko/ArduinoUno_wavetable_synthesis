2ffefd1f45482e738bb1f33ed4efb98235fc9cb7
Keiji Katahira <Keiji Katahira>
2020-06-21 15:12:18 +0900

    sss

--- WaveMemorySyns/envtone_def.h
+++ WaveMemorySyns/envtone_def.h
@@ -1,18 +1,18 @@
 /* 
 *  MAX_TONE 発音数 
-* 16KHZで12ぐらい
+* 16KHZで12
 * 32KHzなら5～6
 * 4以上の発音数を指定するとoverflowした時に雑音が出ます
-* CLIPを指定時発音数が１つぐらい減少
+* CLIPを指定時発音数が1～2減少します
 */
 #define MAX_TONE 5
 
 /*
  * USE_32KZ  32KHzで使用するときはコメントアウトを外してください
  */
-#define USE_32KHZ
+//#define USE_32KHZ
 
 /*
  *   CLIP 波形加算時overflowした時時最大値にCLIPします
  */
-#define CLIP
+//#define CLIP
